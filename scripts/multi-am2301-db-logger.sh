946cc623442682053c380933ab60c00d7207d975
Marcin P <marcin.pil@gmail.com>
2016-12-06 22:15:35 +0100

    Script refactoring

--- scripts/multi-am2301-db-logger.sh
+++ scripts/multi-am2301-db-logger.sh
@@ -1,152 +1,38 @@
 #!/bin/bash
 
-LINE=`grep 23_temperature /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Temperature";
-    exit $?;
-fi
-COLS=( $LINE );
-T=${COLS[2]};
+function log_sensor_data {
+    LINE=`grep $1_temperature /proc/multi-am2301`;
+    if [ $? -ne 0 ]; then
+        echo "Problem with grep $1_temperature";
+        exit $?;
+    fi
+    COLS=( $LINE );
+    T=${COLS[2]};
 
-LINE=`grep 23_RH /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep RH";
-    exit $?;
-fi
+    LINE=`grep $1_RH /proc/multi-am2301`;
+    if [ $? -ne 0 ]; then
+        echo "Problem with grep $1_RH";
+        exit $?;
+    fi
 
-COLS=( $LINE );
-RH=${COLS[2]};
+    COLS=( $LINE );
+    RH=${COLS[2]};
 
-LINE=`grep 23_timestamp /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Timestamp";
-    exit $?;
-fi
-COLS=( $LINE );
-TIMESTAMP=${COLS[2]};
+    LINE=`grep $1_timestamp /proc/multi-am2301`;
+    if [ $? -ne 0 ]; then
+        echo "Problem with grep $1_timestamp";
+        exit $?;
+    fi
+    COLS=( $LINE );
+    TIMESTAMP=${COLS[2]};
 
-DATETIME=`date --date @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S'`;
+    DATETIME=`date --date @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S'`;
 
-sqlite3 /var/local/am2301-db.sl3 "INSERT INTO pin23 VALUES('$DATETIME', '$T', '$RH');";
+    sqlite3 /var/local/am2301-db.sl3 "INSERT INTO $2 VALUES('$DATETIME', '$T', '$RH');";
+}
 
-
-
-LINE=`grep 24_temperature /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Temperature";
-    exit $?;
-fi
-COLS=( $LINE );
-T=${COLS[2]};
-
-LINE=`grep 24_RH /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep RH";
-    exit $?;
-fi
-
-COLS=( $LINE );
-RH=${COLS[2]};
-
-LINE=`grep 24_timestamp /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Timestamp";
-    exit $?;
-fi
-COLS=( $LINE );
-TIMESTAMP=${COLS[2]};
-
-DATETIME=`date --date @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S'`;
-
-sqlite3 /var/local/am2301-db.sl3 "INSERT INTO root_cellar VALUES('$DATETIME', '$T', '$RH');";
-
-
-
-LINE=`grep 25_temperature /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Temperature";
-    exit $?;
-fi
-COLS=( $LINE );
-T=${COLS[2]};
-
-LINE=`grep 25_RH /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep RH";
-    exit $?;
-fi
-
-COLS=( $LINE );
-RH=${COLS[2]};
-
-LINE=`grep 25_timestamp /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Timestamp";
-    exit $?;
-fi
-COLS=( $LINE );
-TIMESTAMP=${COLS[2]};
-
-DATETIME=`date --date @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S'`;
-
-sqlite3 /var/local/am2301-db.sl3 "INSERT INTO pin25 VALUES('$DATETIME', '$T', '');";
-
-
-LINE=`grep 8_temperature /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Temperature";
-    exit $?;
-fi
-COLS=( $LINE );
-T=${COLS[2]};
-
-LINE=`grep 8_RH /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep RH";
-    exit $?;
-fi
-
-COLS=( $LINE );
-RH=${COLS[2]};
-
-LINE=`grep 8_timestamp /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Timestamp";
-    exit $?;
-fi
-COLS=( $LINE );
-TIMESTAMP=${COLS[2]};
-
-DATETIME=`date --date @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S'`;
-
-sqlite3 /var/local/am2301-db.sl3 "INSERT INTO pin8 VALUES('$DATETIME', '$T', '$RH');"
-
-
-LINE=`grep 7_temperature /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Temperature";
-    exit $?;
-fi
-COLS=( $LINE );
-T=${COLS[2]};
-
-LINE=`grep 7_RH /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep RH";
-    exit $?;
-fi
-
-COLS=( $LINE );
-RH=${COLS[2]};
-
-LINE=`grep 7_timestamp /proc/multi-am2301`;
-if [ $? -ne 0 ]; then
-    echo "Problem with grep Timestamp";
-    exit $?;
-fi
-COLS=( $LINE );
-TIMESTAMP=${COLS[2]};
-
-DATETIME=`date --date @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S'`;
-
-sqlite3 /var/local/am2301-db.sl3 "INSERT INTO pin7 VALUES('$DATETIME', '$T', '$RH');"
+log_sensor_data 23 pin23
+log_sensor_data 24 root_cellar
+log_sensor_data 25 pin25
+log_sensor_data 8 pin8
+log_sensor_data 7 pin7
