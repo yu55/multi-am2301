b5c5922a611e2d68e58c7750d379eb964e9cfe4b
Marcin P <marcin.pil@gmail.com>
2016-12-31 10:44:04 +0100

    Refactoring: validation->eviction

--- module/stats.c
+++ module/stats.c
@@ -27,21 +27,21 @@ void stats_update(MEASUREMENT measurement) {
 }
 
 void evict_outdated_in_fifo_1m(time_t current_time) {
-    bool continue_validation = true;
+    bool continue_eviction = true;
     MEASUREMENT m;
     m.temp = -1;
     m.timestamp = -1;
     printk(KERN_INFO "queue len: %u\n", kfifo_len(&fifo_1m));
 
-    while(continue_validation) {
-        continue_validation = false;
+    while(continue_eviction) {
+        continue_eviction = false;
         if (kfifo_peek(&fifo_1m, &m)) {
             printk(KERN_INFO "peek: %d,%d,%ld\n", m.pin_index, m.temp, m.timestamp);
 
             if (current_time - m.timestamp > 60) {
                 if (kfifo_get(&fifo_1m, &m)) {
                     printk(KERN_INFO "get: %d,%d,%ld\n", m.pin_index, m.temp, m.timestamp);
-                    continue_validation = true;
+                    continue_eviction = true;
                 }
             }
         }
