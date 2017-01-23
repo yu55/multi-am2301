734f3674570015912b74480b3f46550e42cab5d2
Marcin P <marcin.pil@gmail.com>
2017-01-23 21:17:41 +0100

    Formatting: indent -linux

--- module/stats.h
+++ module/stats.h
@@ -4,13 +4,13 @@
 #include <linux/ktime.h>
 
 typedef struct MEASUREMENT {
-    unsigned short int pin_index;
-    short int temp;
-    unsigned short int rh;
-    time_t timestamp;
+	unsigned short int pin_index;
+	short int temp;
+	unsigned short int rh;
+	time_t timestamp;
 } MEASUREMENT;
 
 void stats_init(int temp_1m[], int rh_1m[], unsigned short int _1m_size);
 void stats_update(MEASUREMENT measurement);
 
-#endif 
+#endif
